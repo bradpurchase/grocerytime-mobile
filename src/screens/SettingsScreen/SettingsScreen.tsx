41eb6500de18fc2040dcefeb982d47da4b955c5a
Brad Purchase <bradpurchase@gmail.com>
2020-06-01 07:42:59 -0400

    Update SettingsScreen.tsx

--- src/screens/SettingsScreen/SettingsScreen.tsx
+++ src/screens/SettingsScreen/SettingsScreen.tsx
@@ -124,7 +124,7 @@ const SettingsScreen: React.FC<Props> = React.memo(({ navigation }) => {
                 marginTop: 30,
                 textAlign: 'center',
               }}>
-              GroceryTime 1.0.0 beta 1
+              GroceryTime 1.0.0 beta 2
             </Text>
           </>
         )}
